997bf2d6343b83dab5bc4587cd4e317115e745c6
jd <duverseaujohnny21@gmail.com>
2020-09-02 19:34:26 -0400

    add pga-game file
    Co-authored-by: Donny Mays <dtmays2@gmail.com>

---
+++ src/pga-game.js
@@ -0,0 +1,14 @@
+export function Wrestler(strength, charisma, introSong, health) {
+  this.strength = strength;
+  this.charisma = charisma;
+  this.introSong = introSong;
+  this.health = health;
+}
+
+//BamBam(75, 55, 100, 75) vs ScaryMan(50, 100, 75, 100)
+
+
+Wrestler.prototype.attack = function() {
+//BamBam = new Wrestler(75, 55, 100)
+}
+
